75e9269f8bd6795c214efb0173892117565ca4bb
Paul Dib <paul.dib@orange.fr>
2014-12-15 17:13:21 +0100

    Splitting DocumentIndex.

--- index/document_index.py
+++ index/document_index.py
@@ -1,36 +1,48 @@
 from .utility import *
 
-class DocumentIndex:
-    '''Class containing the indexing result for one document.'''
+class Document:
+    '''
+    Class representing one document for read access.
+    '''
     def __init__(self, content, indexConfig):
         self._config = indexConfig
-        self.wordCount = {}
-        self._getFieldPositions(content)
-        self._initIndex(content)
-        
-    def getWordCount(self):
-        return self.wordCount
+        self._content = content
+        self._getFieldPositions()
 
-    def getFieldContent(self, field, documentContent):
+    def getFieldContent(self, field):
         startPos = self.fieldPositions[field] + 1
         if (startPos <= 0):
             return ""
         nextFields = {v for (k,v) in self.fieldPositions.items() if v > startPos}
-        stopPos = min(nextFields) if nextFields else len(documentContent)
-        return "\n".join(documentContent[startPos:stopPos])
+        stopPos = min(nextFields) if nextFields else len(self._content)
+        return "\n".join(self._content[startPos:stopPos])
 
-    def _getFieldPositions(self, content):
+    def _getFieldPositions(self):
         '''Populates fieldPositions = { 'fieldName': startPosition } dictionary.'''
         self.fieldPositions = {}
         for field in self._config.fields:
-            self.fieldPositions[field] = next((i for i, l in enumerate(content) if l.startswith(field)), -1)
+            self.fieldPositions[field] = next((i for i, l in enumerate(self._content) if l.startswith(field)), -1)
 
-    def _initIndex(self, content):
+
+class DocumentIndex:
+    '''
+    Class containing the indexing result for one document.
+    '''
+    def __init__(self, content, indexConfig):
+        self._config = indexConfig
+        self.wordCount = {}
+        self._doc = Document(content, indexConfig)
+        self._initIndex()
+
+    def getWordCount(self):
+        return self.wordCount
+
+    def _initIndex(self):
         for field in self._config.focusFields:
-            self.wordCount = mergeDictionaries(self.wordCount, self._getWordCount(field, content))
+            self.wordCount = mergeDictionaries(self.wordCount, self._getWordCount(field))
 
-    def _getWordCount(self, field, content):
-        fieldContent = self.getFieldContent(field, content)
+    def _getWordCount(self, field):
+        fieldContent = self._doc.getFieldContent(field)
         return countTokens(self._tokenize(fieldContent))
 
     def _tokenize(self, content):
--- index/tests/test_document_index.py
+++ index/tests/test_document_index.py
@@ -1,6 +1,6 @@
 import unittest
 import os
-from index.document_index import DocumentIndex
+from index.document_index import DocumentIndex, Document
 from index.index_config import IndexConfig
 
 class DocumentIndexTests(unittest.TestCase):
@@ -8,15 +8,16 @@ class DocumentIndexTests(unittest.TestCase):
         with open(os.path.dirname(os.path.realpath(__file__)) + "/test_data") as file:
             self.testContent = file.read().splitlines()
         self.docIndex = DocumentIndex(self.testContent, IndexConfig())
+        self.doc = Document(self.testContent, IndexConfig())
 
     def test_getFieldPositions(self):
         expected = {'.A': 5, '.B': 3, '.I': 0, '.K': -1, '.N': 8, '.T': 1, '.W': -1, '.X': 10}
-        actual = self.docIndex.fieldPositions
+        actual = self.doc.fieldPositions
         self.assertEqual(expected, actual)
 
     def test_getFieldContent(self):
         expected = "Perlis, A. J.\nSamelson,K."
-        actual = self.docIndex.getFieldContent(".A", self.testContent)
+        actual = self.doc.getFieldContent(".A")
         self.assertEqual(expected, actual)
 
     def test_createIndex(self):
