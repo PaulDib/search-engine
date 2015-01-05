01a94534f4c72c77ec8a7413502d012908568b5a
Paul Dib <paul.dib@orange.fr>
2015-01-05 22:04:46 +0100

    Adding a script to compute statistics on the search engine.

---
+++ stats.py
@@ -0,0 +1,101 @@
+from index.index import Index
+from index.index_config import IndexConfig
+from index.vectorial_query import VectorialQueryTfIdf, VectorialQueryNormCount
+from math import floor
+from pylab import *
+
+def read_queries(file_path):
+    queries = {}
+    query_id = 0
+    with open(file_path) as file_ptr:
+        for line in file_ptr:
+            if line.startswith('.I'):
+                query_id = int(line.split(" ")[1])
+            elif line.startswith('.W'):
+                query_buffer = ""
+            elif line.startswith('.N'):
+                queries[query_id] = query_buffer
+            else:
+                query_buffer = query_buffer + line
+    return queries
+
+
+def read_expected_results(file_path):
+    expected = {}
+    with open(file_path) as file_ptr:
+        for line in file_ptr:
+            splitted = line.split(" ")
+            query_id = int(splitted[0])
+            expected_doc = int(splitted[1])
+            if query_id in expected:
+                expected[query_id].append(expected_doc)
+            else:
+                expected[query_id] = [expected_doc]
+    return expected
+
+
+print('Indexing...')
+config = IndexConfig('data/common_words')
+index = Index('data/cacm.all', config)
+
+queries = read_queries('data/query.text')
+expected = read_expected_results('data/qrels.text')
+results_tfidf = {}
+results_norm_count = {}
+
+print('Running queries...')
+
+average_prec_list = []
+average_recall_list = []
+
+for percentage in range(1, 100, 1):
+    average_prec = 0
+    average_recall = 0
+    for (query_id, query_text) in queries.items():
+        pertinent = expected[query_id] if query_id in expected else []
+        pertinent_len = len(pertinent)
+
+        query_tfidf = VectorialQueryNormCount(query_text)
+        results_tfidf[query_id] = query_tfidf.execute(index)
+        tfidf_len = len(results_tfidf[query_id])
+
+        top = math.floor(tfidf_len*percentage/100)
+        results_tfidf[query_id] = results_tfidf[query_id][0:top]
+        
+        tfidf_found = len([k for k in pertinent if k in [x for (x, y) in results_tfidf[query_id]]])
+        tfidf_recall = tfidf_found / pertinent_len if pertinent_len > 0 else 1.0
+        tfidf_prec = tfidf_found / tfidf_len
+        
+        average_prec = average_prec + tfidf_prec
+        average_recall = average_recall + tfidf_recall
+    average_prec = average_prec / len(queries)
+    average_recall = average_recall / len(queries)
+    average_prec_list.append(average_prec)
+    average_recall_list.append(average_recall)
+
+print(average_prec_list)
+print(average_recall_list)
+
+plot(average_recall_list, average_prec_list)
+
+xlabel('recall')
+ylabel('precision')
+title('normalized_count')
+grid(True)
+savefig("images/normalized_count.png")
+show()
+
+
+
+# query_norm_count = VectorialQueryNormCount(query_text)
+# results_norm_count[query_id] = query_norm_count.execute(index)
+# norm_count_len = len(results_norm_count[query_id])
+# norm_count_found = len([k for k in pertinent if k in [x for (x, y) in results_norm_count[query_id]]])
+# norm_count_recall = norm_count_found / pertinent_len if pertinent_len > 0 else 1.0
+# norm_count_prec = norm_count_found / norm_count_len
+
+# print('q{0} tfidf r:{1:.3f} p:{2:.3f} - n_cnt r:{3:.3f} p:{4:.3f}'.format(
+#     query_id, tfidf_recall, tfidf_prec, norm_count_recall, norm_count_prec))
+
+
+    
