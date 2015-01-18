302e665a7dd3f14be20f4e291b85938fde23549d
Paul Dib <paul.dib@orange.fr>
2015-01-18 20:39:44 +0100

    adding a fake stemmer to compare with porter stemmer results.

--- index/core/configuration.py
+++ index/core/configuration.py
@@ -5,6 +5,15 @@ from nltk import PorterStemmer
 from .document_parser import CACMDocumentParser
 
 
+class FakeStemmer:
+    def __init__(self):
+        pass
+
+    def stem_word(self, word):
+        '''Doesn't really stem anything.'''
+        return word
+
+
 class Configuration(object):
 
     '''Exposes configuration items for the index core package.'''
